7a9c85b1afa01a5d76707ca05be7779a1b1135bf
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-07 22:42:54 +0200

    feat: allow the baseIRI to be provided (#33)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,7 @@ import { Quad } from '@rdfjs/types';
 
 export interface ParseOptions {
   extendedSyntax?: boolean;
+  baseIRI?: string;
 }
 
 export declare function parse(str: string, options?: ParseOptions): Quad[] & { prefixes: Record<string, string> };
