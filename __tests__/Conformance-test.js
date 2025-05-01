13106dd5c9e1973cc13653bccc21ba0e93b64ba6
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-01 20:52:03 +0100

    chore: fixup tests

--- __tests__/Conformance-test.js
+++ __tests__/Conformance-test.js
@@ -34,6 +34,7 @@ describe('Testing each extended conformance file', () => {
     expect(res).toBeRdfIsomorphic((new N3.Parser()).parse(ttl))
     expect(res.prefixes).toEqual({
       rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      owl: 'http://www.w3.org/2002/07/owl#',
       rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
       sh: 'http://www.w3.org/ns/shacl#',
       xsd: 'http://www.w3.org/2001/XMLSchema#',
